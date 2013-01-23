4f3b2fa8d85c99461a8a0f4307ace1f3940d696f
Christopher Valles <info@christophervalles.com>
2013-01-23 08:25:54 +0000

    First pass chapter 4

---
+++ module/Wall/src/Wall/PostProcessor/json.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wall\PostProcessor;
+
+/**
+ *
+ */
+class Json extends AbstractPostProcessor
+{
+    public function process()
+    {
+        $result = \Zend\Json\Encoder::encode($this->_vars);
+        $this->_response->setContent($result);
+
+        $headers = $this->_response->getHeaders();
+        $headers->addHeaderLine('Content-Type', 'application/json');
+        $this->_response->setHeaders($headers);
+    }
+}
